97819006da2a5202de10ce3cf1c535260b54aaff
HENRIQUE DA SILVA VERISSIMO DOS SANTOS <henrique.svsantos1@senacsp.edu.br>
2024-10-21 16:21:53 -0300

    V. 1.0

---
+++ backup.bat
@@ -0,0 +1,4 @@
+mkdir BACKUP
+copy  /windows/*.txt
+copy  /windows/system32/*.log /users/henrique.svsantos1/documents/script-backup/BACKUP
+copy  /windows/systerm32/*.txt /users/henrique.svsantos1/documents/script-backup/BACKUP
